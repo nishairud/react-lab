964cff0b6d412b9595c13a8aa1317115fe316df4
Nisha <nishairud@gmail.com>
2018-06-26 19:07:02 -0700

    Initial changes to display HelloWorld

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// eslint-disable-next-line
+import logo from './logo.svg';
+import Proj from './Proj.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hellooooooo worlddd!!!!</h1>
+        <h2>hiii</h2>
+        <h3> Welcome to my first react project</h3>
+        <Proj/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Proj.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// eslint-disable-next-line
+import logo from './logo.svg';
+import './App.css';
+
+class Proj extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Hellooooooo everyone!!!!</h1>
+      <h2>hiii</h2>
+      <h3> Welcome to my second react project</h3>
+      </div>
+
+    );
+  }
+}
+
+export default Proj;
